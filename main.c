a21508315e4fa7eb10db7c159e467c0360e9b716
Thomas Versteeg <thomasversteeg@gmx.com>
2015-06-10 20:28:30 +0200

    Add simple error catching and signal interruption

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,11 +12,24 @@
 #define CHUNK_SIZE 256
 #define SEND_TIMEOUT 10
 
-int port;
+#define RTSP_STATUS_200 "RTPS/1.0 200 OK\r\n"
+#define RTSP_STATUS_CSEQ "CSEQ: %d\r\n"
+#define RTSP_STATUS_OPTIONS "Public: DESCRIBE, SETUP, TEARDOWN, PLAY, PAUSE\r\n"
+
+int port, fd, childfd;
+
+void stop(int sig)
+{
+	printf("\n%d - Closing server\n", sig);
+
+	close(fd);
+	close(childfd);
+	exit(0);
+}
 
 int main(int argc, char *argv[])
 {
-	port = 9403;
+	port = 554;
 
 	int i;
 	for(i = 1; i < argc; i++){
@@ -30,9 +45,11 @@ int main(int argc, char *argv[])
 
 	printf("Port: %d\n", port);
 
-	int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	childfd = 0;
+	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if(fd < 0){
 		perror(NULL);
+		close(fd);
 		return 1;
 	}
 
@@ -43,51 +60,63 @@ int main(int argc, char *argv[])
 
 	int yes = 1;
 	if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0){
-		perror(NULL);
-		return 1;
+		goto error;
 	}
 
 	if(bind(fd, (struct sockaddr*)&servaddr, sizeof(struct sockaddr_in)) < 0){
-		perror(NULL);
-		return 1;
+		goto error;
 	}
 
 	if(listen(fd, MAX_CONNECTIONS) < 0){
-		perror(NULL);
-		return 1;
+		goto error;
 	}
+
+	signal(SIGINT, stop);
 
 	while(1){
 		struct sockaddr_in cliaddr;
 		unsigned int clilen = sizeof(cliaddr);
-		int newfd = accept(fd, (struct sockaddr*)&cliaddr, &clilen);
-		if(newfd < 0){
-			perror(NULL);
-			return 1;
+		childfd = accept(fd, (struct sockaddr*)&cliaddr, &clilen);
+		if(childfd < 0){
+			goto error;
 		}
 
-		fcntl(newfd, F_SETFL, O_NONBLOCK);
+		fcntl(childfd, F_SETFL, O_NONBLOCK);
 		while(1){
 			char data[CHUNK_SIZE];
-			int status = recv(newfd, data, CHUNK_SIZE, 0);
+			int status = recv(childfd, data, CHUNK_SIZE, 0);
 			if(status < 0){
 				usleep(10000);
 			}else if(status > 0){
 				data[status] = '\0';
 				printf("%s", data);
+
 				if(status < CHUNK_SIZE){
-					close(fd);
-					return 0;
+					// Received full message
+					char options[1024];
+					int optsize = sprintf(options, RTSP_STATUS_200 RTSP_STATUS_CSEQ RTSP_STATUS_OPTIONS, 1);
+					if(write(childfd, options, optsize) != optsize){
+						goto error;
+					}
+					
+					break;
 				}
 			}
 		}
 
-		fcntl(newfd, F_SETFL, fcntl(newfd, F_GETFL) & ~O_NONBLOCK);
+		fcntl(childfd, F_SETFL, fcntl(childfd, F_GETFL) & ~O_NONBLOCK);
 
-		close(newfd);
+		close(childfd);
 	}
 
 	close(fd);
 
 	return 0;
+
+error:
+	perror(NULL);
+
+	close(fd);
+	close(childfd);
+	return 1;
 }
